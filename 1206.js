fe5efd03fe343f12d8ff6815ad3631f72300a106
Joy <joyhsuu7@gmail.com>
2023-12-12 19:32:01 +0800

    feat: update

--- 1206.js
+++ 1206.js
@@ -1,21 +1,21 @@
-window.onload = function () {
-  setInterval(time, 1000);
-};
+// window.onload = function () {
+//   setInterval(time, 1000);
+// };
 
-function time() {
-  let date = new Date();
+// function time() {
+//   let date = new Date();
 
-  let year = date.getFullYear();
-  let month = date.getMonth() + 1;
-  let day = date.getDate();
-  let hour = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  let dayOfWeek = date.getDay(); // 获取星期几
+//   let year = date.getFullYear();
+//   let month = date.getMonth() + 1;
+//   let day = date.getDate();
+//   let hour = date.getHours();
+//   let minutes = date.getMinutes();
+//   let seconds = date.getSeconds();
+//   let dayOfWeek = date.getDay(); // 获取星期几
 
-  let daysOfWeekArray = ["日", "一", "二", "三", "四", "五", "六"];
-  let dayOfWeekString = daysOfWeekArray[dayOfWeek];
+//   let daysOfWeekArray = ["日", "一", "二", "三", "四", "五", "六"];
+//   let dayOfWeekString = daysOfWeekArray[dayOfWeek];
 
-  let time = `${year}年 ${month}月 ${day}日 星期${dayOfWeekString} ${hour}时 ${minutes}分 ${seconds}秒 `;
-  document.getElementById("time").innerHTML = time;
-}
+//   let time = `${year}年 ${month}月 ${day}日 星期${dayOfWeekString} ${hour}时 ${minutes}分 ${seconds}秒 `;
+//   document.getElementById("time").innerHTML = time;
+// }
